b7711339d9a9134d3396f44756264a40477f3fb2
Jimmy <jlrichii@gmail.com>
2024-04-18 16:07:20 -0500

    Submit handler the vue3 wayyy

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,28 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+var formData = ref({
+  url: null,
+  submitted: false,
+});
+
 function handleSubmit() {
   console.log('Form submitted');
+  formData.value.submitted = true;
 }
 </script>
 
 <template>
   <main>
-    <form @submit.prevent="handleSubmit()" class="p-24">
-      <input type="url" class="bg-transparent border-white border-b-2 p-4" placeholder="Enter URL..." />
+    <form v-if="!formData.submitted" @submit.prevent="handleSubmit()" class="p-24">
+      <input v-model="formData.url" type="url" class="bg-transparent border-white border-b-2 p-4"
+        placeholder="Enter URL..." />
       <button type="submit" class="p-4 bg-slate-200 text-slate-950">
         Next
       </button>
     </form>
+    <div v-else class="p-24">
+      <p>Form submitted! {{ formData.url }}</p>
+    </div>
   </main>
 </template>
